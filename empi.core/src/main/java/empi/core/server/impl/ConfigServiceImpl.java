d186be8509d69cd710005807d849dea00d137bdd
pxg950110@163.com <pxg950110@163.com>
2020-11-26 22:36:48 +0800

    代码增加

--- empi.core/src/main/java/empi/core/server/impl/ConfigServiceImpl.java
+++ empi.core/src/main/java/empi/core/server/impl/ConfigServiceImpl.java
@@ -1,6 +1,7 @@
 package empi.core.server.impl;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import empi.core.dao.MatchFactorMapper;
@@ -27,8 +29,8 @@ import empi.core.utils.HttpResultStatus;
 
 /**
  * @author chpxg
- * @see 配置服务的实现类
  * @version v1.0.0
+ * @description 配置服务的实现类
  */
 @Service
 public class ConfigServiceImpl implements ConfigService {
@@ -128,89 +130,121 @@ public class ConfigServiceImpl implements ConfigService {
 
     @Override
     public CommonResult<Object> operationMatchFactor(MatchFactor matchFactor) {
-        String uid=CommonUtils.uuid();
-        // 属性code 属性name 加分 减分
-        //添加或修改属性和分值
-        //获取所有属性
-        Map<String,String> propertyMap=EmpiProtertyCache.getPropertyMap();
-        //如果属性不存在 检查或重启服务后再调试
-        if(propertyMap.get(matchFactor.getPropertyCode())==null){
-            return CommonResult.commomResult("属性不存在"+matchFactor.getPropertyCode()+"---"+matchFactor.getPropertyName()+",请重新检查或重启服务",HttpResultStatus.STATUS500)
-        }
-        //如果属性存在  
-        //获取属性id code name
-        MatchPropertyExample example=new MatchPropertyExample();
-        example.createCriteria().andCodeEqualTo(matchFactor.getPropertyCode()).andIsDeletedEqualTo((short) 0);
-        MatchProperty matchProperty=matchPropertyMapper.selectByExample(example).get(0);
-        //设置属性ID
-        matchFactor.setPropertyId(matchProperty.getId());
-        //设置属性名称
-        matchFactor.setPropertyName(matchProperty.getName());
-         // 加分值判断
-         Integer positiveGrede=matchFactor.getPositiveGrede();
-         if(positiveGrede==null || positiveGrede<0 ){
-             return  CommonResult.commomResult("加分值允许值为正数", HttpResultStatus.STATUS300);
-         }
-         //减分 分值 更新也必须要有减分
-         Integer negativeGrade=matchFactor.getNegativeGrade();
-         if(negativeGrade==null || negativeGrade<0){
-           //设置分值为0
-           matchFactor.setNegativeGrade(0);
-         }
-        
-        //加入验证是否存在数据 存在更新  不存在 插入
-        //
-        MatchFactorExample matchFactorExample=new MatchFactorExample();
-        //创建 code  is_deleted的查询
-        matchFactorExample.createCriteria().andPropertyCodeEqualTo(matchFactor.getPropertyCode()).andIsDeletedEqualTo((short) 0);
-        List<MatchFactor> matchFactors=matchFactorMapper.selectByExample(matchFactorExample);
-        //
-        if(matchFactors.size()>0){
-            MatchFactor oldMatchFactor=matchFactors.get(0);
-            //设置属性开始
-            //
-            matchFactor.setCreateTime(oldMatchFactor.getCreateTime());
-            matchFactor.setCreateBy(oldMatchFactor.getCreateBy());
-            matchFactor.setId(oldMatchFactor.getId());
-            if(matchFactor.getUpdateBy()==null){
-                matchFactor.setUpdateBy(1);
+        String uid = CommonUtils.uuid();
+        try {
+            // 属性code 属性name 加分 减分
+            //添加或修改属性和分值
+            //获取所有属性
+            Map<String, String> propertyMap = EmpiProtertyCache.getPropertyMap();
+            //如果属性不存在 检查或重启服务后再调试
+            if (propertyMap.get(matchFactor.getPropertyCode()) == null) {
+                return CommonResult.commomResult("属性不存在" + matchFactor.getPropertyCode() + "---"
+                        + matchFactor.getPropertyName() + ",请重新检查或重启服务", HttpResultStatus.STATUS500);
             }
-            matchFactor.setLastUpdateTime(new Date());
-            matchFactor.setIsDeleted((short) 0);
-            matchFactor.setThreshold(oldMatchFactor.getThreshold());
-            matchFactor.setMatchFunction(oldMatchFactor.getMatchFunction());
-            matchFactor.setMatchCondition(oldMatchFactor.getMatchCondition());
-            //更新数据()
-            matchFactor.setSort(oldMatchFactor.getSort());
-            matchFactorMapper.updateByPrimaryKey(matchFactor);
-            return CommonResult.commomResult("规则修改成功", HttpResultStatus.STATUS200);
+            //如果属性存在
+            //获取属性id code name
+            MatchPropertyExample example = new MatchPropertyExample();
+            example.createCriteria().andCodeEqualTo(matchFactor.getPropertyCode()).andIsDeletedEqualTo((short) 0);
+            MatchProperty matchProperty = matchPropertyMapper.selectByExample(example).get(0);
+            //设置属性ID
+            matchFactor.setPropertyId(matchProperty.getId());
+            //设置属性名称
+            matchFactor.setPropertyName(matchProperty.getName());
+            // 加分值判断
+            Integer positiveGrede = matchFactor.getPositiveGrede();
+            if (positiveGrede == null || positiveGrede < 0) {
+                return CommonResult.commomResult("加分值允许值为正数", HttpResultStatus.STATUS300);
+            }
+            //减分 分值 更新也必须要有减分
+            Integer negativeGrade = matchFactor.getNegativeGrade();
+            if (negativeGrade == null || negativeGrade < 0) {
+                //设置分值为0
+                matchFactor.setNegativeGrade(0);
+            }
 
-        }else
-        {
-            if(matchFactor.getCreateBy()==null){
-                matchFactor.setCreateBy(1);
+            //加入验证是否存在数据 存在更新  不存在 插入
+            //
+            MatchFactorExample matchFactorExample = new MatchFactorExample();
+            //创建 code  is_deleted的查询
+            matchFactorExample.createCriteria().andPropertyCodeEqualTo(matchFactor.getPropertyCode()).andIsDeletedEqualTo((short) 0);
+            List<MatchFactor> matchFactors = matchFactorMapper.selectByExample(matchFactorExample);
+            //
+            if (matchFactors.size() > 0) {
+                MatchFactor oldMatchFactor = matchFactors.get(0);
+                //设置属性开始
+                matchFactor.setCreateTime(oldMatchFactor.getCreateTime());
+                matchFactor.setCreateBy(oldMatchFactor.getCreateBy());
+                matchFactor.setId(oldMatchFactor.getId());
+                if (matchFactor.getUpdateBy() == null) {
+                    matchFactor.setUpdateBy(1);
+                }
+                matchFactor.setLastUpdateTime(new Date());
+                matchFactor.setIsDeleted((short) 0);
+                matchFactor.setThreshold(oldMatchFactor.getThreshold());
+                matchFactor.setMatchFunction(oldMatchFactor.getMatchFunction());
+                matchFactor.setMatchCondition(oldMatchFactor.getMatchCondition());
+                //更新数据()
+                matchFactor.setSort(oldMatchFactor.getSort());
+                matchFactorMapper.updateByPrimaryKey(matchFactor);
+                return CommonResult.commomResult("规则修改成功", HttpResultStatus.STATUS200);
+
+            } else {
+                //新数据插入
+                if (matchFactor.getCreateBy() == null) {
+                    matchFactor.setCreateBy(1);
+                }
+                matchFactor.setCreateTime(new Date());
+                if (matchFactor.getUpdateBy() == null) {
+                    matchFactor.setUpdateBy(1);
+                }
+                matchFactor.setLastUpdateTime(new Date());
+                //v1.0.0不加入函数和condition 配置匹配度为1
+                matchFactor.setThreshold(BigDecimal.valueOf(1));
+                return CommonResult.commomResult("规则写入成功", HttpResultStatus.STATUS200);
             }
-            matchFactor.setCreateTime(new Date());
-            if(matchFactor.getUpdateBy()==null){
-                matchFactor.setUpdateBy(1);
-            }
-            matchFactor.setLastUpdateTime(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return CommonResult.commomResult(e.getMessage(), HttpResultStatus.STATUS500);
         }
-        
-       
-       
-        return null;
     }
 
+    @Transactional
     @Override
     public CommonResult<Object> operationMatchFactor(List<MatchFactor> matchFactors) {
-
-        return null;
+        List<MatchFactor> errors = new ArrayList<>();
+        List<MatchFactor> success = new ArrayList<>();
+        try {
+            //批量添加
+            matchFactors.forEach(element -> {
+                CommonResult commonResult = operationMatchFactor(element);
+                if (commonResult.getCode() == 200) {
+                    //成功数组
+                    success.add(element);
+                } else {
+                    //失败数组
+                    errors.add(element);
+                }
+            });
+            return CommonResult.commomResult("成功数量：" + success.size()
+                    + ",失败数量：" + errors.size(), HttpResultStatus.STATUS200);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return CommonResult.commomResult(e.getMessage(), HttpResultStatus.STATUS500);
+        }
     }
 
     @Override
     public CommonResult<Object> getAllMatchFator() {
-        return null;
+        //获取所有的配置
+        try {
+            //查询状态有效的数据
+            MatchFactorExample example = new MatchFactorExample();
+            example.createCriteria().andIsDeletedEqualTo((short) 1);
+            return CommonResult.commomResult(matchFactorMapper.selectByExample(example), HttpResultStatus.STATUS200);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return CommonResult.commomResult(e.getMessage(), HttpResultStatus.STATUS500);
+        }
     }
 
     @Override
